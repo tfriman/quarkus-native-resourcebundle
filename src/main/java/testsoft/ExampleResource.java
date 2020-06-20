eabfaf97a93377bf103666ac1e0a12be405ef3fb
Timo Friman <tfriman@redhat.com>
2020-06-20 13:24:08 +0300

    Added resource bundles and config for that.

--- src/main/java/testsoft/ExampleResource.java
+++ src/main/java/testsoft/ExampleResource.java
@@ -1,16 +1,26 @@
 package testsoft;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-@Path("/hello")
+@Path("/")
 public class ExampleResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleResource.class);
 
     @GET
+    @Path("/{lang}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String get(@PathParam("lang") String lang) {
+        ResourceBundle testbundle = ResourceBundle.getBundle("testbundle", new Locale(lang));
+        LOGGER.info("get({}) using bundle: {}", lang, testbundle);
+        return testbundle.getString("testkey");
     }
 }
